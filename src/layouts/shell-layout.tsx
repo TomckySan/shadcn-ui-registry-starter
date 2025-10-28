39dbcf2279298cc03988734a370c1b1735d861a7
Tomoka Baba <tomoka.baba@route06.co.jp>
2025-10-28 20:03:59 +0900

    update.

--- src/layouts/shell-layout.tsx
+++ src/layouts/shell-layout.tsx
@@ -1,10 +1,6 @@
 import { Geist, Geist_Mono, Montserrat } from "next/font/google";
 import React, { type ReactNode } from "react";
 
-import { BrandHeader } from "@/components/brand-header";
-import { BrandSidebar } from "@/components/brand-sidebar";
-import { SidebarProvider } from "@/components/ui/sidebar";
-import { Toaster } from "@/components/ui/sonner";
 import { cn } from "@/lib/utils";
 
 import "@/app/globals.css";
@@ -40,14 +36,9 @@ export default function ShellLayout({
       )}
     >
       <body>
-        <SidebarProvider>
-          <BrandHeader />
-          <BrandSidebar />
-          <main className="mt-16 flex w-full justify-center">
-            <div className="container">{children}</div>
-          </main>
-          <Toaster />
-        </SidebarProvider>
+        <main className="flex w-full justify-center">
+          <div className="container">{children}</div>
+        </main>
       </body>
     </html>
   );
